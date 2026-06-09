25fe1b92d2b6bfcdc95effb1e096c2b264ba1052
Jaanus198080 <krediitas@gmail.com>
2026-06-09 13:41:57 +0200

    Add Firebase initialization and Firestore setup

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// src/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZCkPOtI5mB5unXSyB4IsHhBz3zvxJWRs",
+  authDomain: "autotrack-ffa8a.firebaseapp.com",
+  projectId: "autotrack-ffa8a",
+  storageBucket: "autotrack-ffa8a.firebasestorage.app",
+  messagingSenderId: "498694180940",
+  appId: "1:498694180940:web:be11daf50017c7ce8c64e9"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
